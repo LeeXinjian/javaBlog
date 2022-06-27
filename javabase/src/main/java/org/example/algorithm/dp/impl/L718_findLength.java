ad4552eabbad21892f7d3ebfed0057baf3b660c0
LeeXinJian <taoanqi@xiaomi.com>
2022-06-27 20:32:34 +0800

    GuavaRateLimiter

--- javabase/src/main/java/org/example/algorithm/dp/impl/L718_findLength.java
+++ javabase/src/main/java/org/example/algorithm/dp/impl/L718_findLength.java
@@ -1,21 +1,90 @@
-package org.example;
+package org.example.algorithm.dp.impl;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Main {
+public class L718_findLength {
 
     public static void main(String[] args) {
         int[] a = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int[] b = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+
         System.out.println(
-                new Main().findLengthDP(a, b)
+                new L718_findLength().findLengthDP(a, b)
+        );
+
+        System.out.println(
+                new L718_findLength().findLengthForce(a, b)
         );
     }
 
-    public int findLength(int[] nums1, int[] nums2) {
+    /**
+     * 滑动窗口
+     */
+    public int findLengthDPslidingWindow(int[] nums1, int[] nums2) {
+        if (nums1.length == 0 || nums2.length == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        int num1Index = 0;
+        int num2Index = nums2.length - 1;
+
+        while (num1Index < nums1.length) {
+            while (num2Index >= 0) {
+                // 遇见值相等
+                int tmp1 = num1Index;
+                int tmp2 = num2Index;
+
+                // 当前轮结果
+                int curResult = 0;
+                while (tmp1 < nums1.length && tmp2 >= 0 && nums1[tmp1] == nums2[tmp2]) {
+                    curResult++;
+                    tmp1++;
+                    tmp2++;
+                }
+
+                result = Math.max(curResult, result);
+                num2Index--;
+            }
+            num1Index++;
+        }
+
+
+        return result;
+    }
+
+    /**
+     * dp
+     */
+    public int findLengthDP(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return 0;
+        }
+
+        // dp[i][j] 表示从0-i,0-j 相等的长度
+        int[][] dp = new int[nums1.length][nums2.length];
+        int result = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                        result = Math.max(dp[i][j], result);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int findLengthForce(int[] nums1, int[] nums2) {
         if (nums1 == null || nums2 == null) {
             return 0;
         }
@@ -73,7 +142,7 @@ public class Main {
     }
 
 
-    public int calCommonLength(int[] shortArray, int[] longArray, int shortIndex, int longIndex) {
+    private int calCommonLength(int[] shortArray, int[] longArray, int shortIndex, int longIndex) {
         int length = 0;
         // 遍历到第一个不等的地方
         while (shortIndex < shortArray.length
@@ -86,32 +155,5 @@ public class Main {
         // 返回相等的长度
         return length;
     }
-
-
-    public int findLengthDP(int[] nums1, int[] nums2) {
-        if (nums1 == null || nums2 == null) {
-            return 0;
-        }
-
-        // dp[i][j] 表示从0-i,0-j 相等的长度
-        int[][] dp = new int[nums1.length][nums2.length];
-        int result = 0;
-
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
-                        result = Math.max(dp[i][j], result);
-                    }
-                }
-            }
-        }
-
-        return result;
-    }
-
 
 }
